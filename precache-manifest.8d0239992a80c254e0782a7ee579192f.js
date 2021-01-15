f1356217bebf7f69ea3e0e5d72682bebffbc3c52
sirancheevisan <sirancheevishan@gmail.com>
2021-01-15 09:49:00 +0530

    Updates

---
+++ precache-manifest.8d0239992a80c254e0782a7ee579192f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f14a99564f665fd731611ab9325acd54",
+    "url": "/Suresh/index.html"
+  },
+  {
+    "revision": "6a7a1438cc469abafe1a",
+    "url": "/Suresh/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "112ee01c8e67b5137466",
+    "url": "/Suresh/static/js/2.cef3aebf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Suresh/static/js/2.cef3aebf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a7a1438cc469abafe1a",
+    "url": "/Suresh/static/js/main.a24acca7.chunk.js"
+  },
+  {
+    "revision": "9e35999a881d9a89ae4a",
+    "url": "/Suresh/static/js/runtime-main.fa76ca0d.js"
+  }
+]);
